c439b79e4d509f074b6760385cd6db34a09057f7
Aswinn3 <aswinselvan3@gmail.com>
2024-03-20 11:16:20 +0530

    Added source

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+export const Header = ({heading, subHeading}) => {
+    return(
+        <section className="header">
+            <h1 data-testid="heading">{heading}</h1>
+            <p data-testid="subHeading">{subHeading}</p>
+        </section>
+    )
+
+}
